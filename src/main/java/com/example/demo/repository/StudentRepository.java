d1d685313079e8cd879c35fd639186da3ce15b61
Shaurya Bansal <shauryab22@gmail.com>
2025-12-09 15:50:01 +0530

    NotNull - warnings

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -7,5 +7,5 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
+public interface StudentRepository extends JpaRepository<@NotNull Student, @NotNull Long> {
 }
