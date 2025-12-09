1146cfbb72f89c1f14211abecb61618e5e5b7faa
Shaurya Bansal <shauryab22@gmail.com>
2025-12-09 15:49:31 +0530

    notnull - warnings

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Repository;
 
 @Repository
